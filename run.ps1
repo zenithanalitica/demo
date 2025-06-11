526a29d0f901442b548cae549af3d2281a5d3b9b
Cyliann <maksymilian@cych.eu>
2025-06-11 09:39:09 +0200

    build: add run file for windows

---
+++ run.ps1
@@ -0,0 +1,31 @@
+# Exit immediately if a command fails
+$ErrorActionPreference = "Stop"
+
+# Function to clone or pull the latest changes
+function Clone-OrPull {
+    param (
+        [string]$RepoUrl,
+        [string]$TargetDir
+    )
+
+    if (Test-Path "$TargetDir\.git") {
+        Write-Host "Updating $TargetDir..."
+        git -C $TargetDir pull
+    } else {
+        Write-Host "Cloning $RepoUrl into $TargetDir..."
+        git clone $RepoUrl $TargetDir
+    }
+}
+
+# Clone or pull repositories
+Clone-OrPull -RepoUrl "https://github.com/zenithanalitica/data-pipeline" -TargetDir "data-pipeline"
+Clone-OrPull -RepoUrl "https://github.com/zenithanalitica/sentiment-score" -TargetDir "sentiment-score"
+
+# Set environment variables
+$env:NEO4J_URI = "neo4j:7687"
+$env:NEO4J_USERNAME = "neo4j"
+$env:NEO4J_PASSWORD = "verycomplicatedpassword"
+$env:NEO4J_AUTH = "$($env:NEO4J_USERNAME)/$($env:NEO4J_PASSWORD)"
+
+# Run Docker Compose
+docker compose up
